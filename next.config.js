858e29bb90e2957d8d4c084baaca277fdfb73488
redraze <coross3@outlook.com>
2024-01-05 10:33:14 -0800

    add new workflow for building and pushing docker image to dockerhub repo. disable previous workflow for building with nextjs and pushing to github pages.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   output: 'export'
+   // set output to 'standalone' when building with docker,
+   // or set to 'export' when building and deploying to gihub pages
+   output: 'standalone'
+   // output: 'export'
 }
 
 module.exports = nextConfig
