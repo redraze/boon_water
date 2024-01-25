2a0619b79186d8cdf43915641188dd437fda28fe
redraze <coross3@outlook.com>
2024-01-25 10:59:02 -0800

    CI fix 1 -- test db connection

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,4 +1,4 @@
-// import clientPromise from "../../lib/dbConnect";
+import clientPromise from "../../lib/dbConnect";
 import { verifyToken } from "../../lib/tokens";
 import { NextResponse } from "next/server";
 
@@ -11,7 +11,7 @@ export async function POST(req: Request) {
             return NextResponse.json({ users: [], validity });
         };
 
-        // const dbClient = await clientPromise;
+        const dbClient = await clientPromise;
         // const db = dbClient?.db('waterUsersDb');
         // const collection = db?.collection('waterUsers');
         // const cursor = collection?.find({});
