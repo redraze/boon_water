f854bb1d62e558bea4e5a973823e35a4d7bad5ac
redraze <coross3@outlook.com>
2024-02-06 08:38:34 -0800

    forgot to add the abstacted components in the previous commit xD

---
+++ app/components/dataEntry/TableHead.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { quarterType } from "../../lib/commonTypes";
+
+export default function TableHead({ quarter }: { quarter: quarterType }) {
+    const monthsTable = {
+        1: { Q1: 'January', Q2: 'April', Q3: 'July', Q4: 'October' },
+        2: { Q1: 'February', Q2: 'May', Q3: 'August', Q4: 'November' },
+        3: { Q1: 'March', Q2: 'June', Q3: 'September', Q4: 'December' }
+    };
+    
+    return (<>
+        <thead>
+            <tr>
+                <td></td>
+                <td>{ monthsTable[1][quarter] }</td>
+                <td>{ monthsTable[2][quarter] }</td>
+                <td>{ monthsTable[3][quarter] }</td>
+            </tr>
+        </thead>
+    </>);
+};
