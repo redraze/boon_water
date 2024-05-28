88fa79189777c5c431812359d4eb14b261d13a41
redraze <coross3@outlook.com>
2024-05-28 11:09:45 -0700

    add a new button on the /dataEntry endpoint that will print a blank meter readings page for the currently selected year/quarter

--- app/components/dataEntry/UserActions.tsx
+++ app/components/dataEntry/UserActions.tsx
@@ -7,7 +7,18 @@ type UserActionsPropsType = {
     handleSubmit: voidFunc
 };
 
-export default function UserActions({ year, quarter, resetUsage, handleSubmit }: UserActionsPropsType) {
+export default function UserActions(
+    { 
+        year, 
+        quarter, 
+        resetUsage, 
+        handleSubmit 
+    }: UserActionsPropsType
+) {
+    const handlePrint = () => {
+        console.log('TODO');
+    };
+
     return(<>
         <button
             className={ !year || !quarter ?
@@ -31,6 +42,17 @@ export default function UserActions({ year, quarter, resetUsage, handleSubmit }:
             Save changes { !year || !quarter ? <></> : 
                 <>for <b>{ quarter }, { year == 'cur' ? 'current year' : 'previous year'}</b></>
             }
+        </button>
+
+        <button
+            className={ !year || !quarter ? 
+                "border-2 border-gray-400 p-2 bg-gray-200 text-gray-400 rounded-lg text-l uppercase" :
+                "bg-gray-200 border-2 border-sky-500 hover:bg-sky-500 hover:text-white rounded-lg p-2 text-l uppercase"
+            }
+            onClick={ () => handlePrint() }
+            disabled={ !year || !quarter }
+        >
+            Print blank page
         </button>
     </>);
 };
