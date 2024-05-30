74b194604444de20af6a575ff5f007f5d30a45d6
redraze <coross3@outlook.com>
2024-05-30 12:25:47 -0700

    clean up unused imports

--- app/dataEntry/page.tsx
+++ app/dataEntry/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter, usePathname } from "next/navigation";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { getData, patchData } from "../lib/dataEntryFunctions";
 import { 
     quarterType, 
